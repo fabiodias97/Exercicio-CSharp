a905edaf2fe8c8a0166ef957845379175ebebe7c
Fábio  Dias <add.fabio97@gmail.com>
2022-01-20 22:17:24 -0300

    Avaliacao

---
+++ ex03/ex03.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex03
+{
+    class ex03
+    {
+        static void Main(string[] args)
+        {   
+            // OPERAÇOES COM O BITWASE
+            double num1 = 10<<1; // para Dobrar o valor utiliza-se o (" << ")
+            Console.Write("O Número 10 foi DOBRADO 1 vez, e está com valor {0}\n", num1);
+
+
+            double num2 = 10>>1; // para Dividir o valor utiliza-se 0 (" >> ")
+            Console.Write("O Número 10 foi DIVIDIDO 1 vez, e está com valor {0}", num2);
+        }
+    }
+}
---
+++ ex04/ex04.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex04
+{
+    class ex04
+    {
+        enum diaSemana
+            {
+                Domingo=1, Segunda, Terça, Quarta, Quinta, Sexta, Sábado
+            }
+        static void Main()
+        {
+            // Esercício com ENUMERADORES (enum)
+            diaSemana ds = (diaSemana)2;
+            Console.Write("Ahh então o seu Dia Preferido é {0}", ds);
+        }
+    }
+}
