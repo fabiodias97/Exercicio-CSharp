7cd3e6c1ccb5026ca63649664e27835990aa5b9a
Fábio  Dias <add.fabio97@gmail.com>
2022-01-22 13:38:38 -0300

    exercicio

---
+++ ex06/ex06.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ex06
+{
+    class Exercicio06
+    {
+        static void Main()
+        {   // Estamos Fazendo Exercício com os Comando "SWITCH" e com "GOTO"
+            int tempo=0;
+            char escolha;
+
+            inicio:
+            Console.Clear();
+
+            Console.Write("De Belo Horizonte/MG até Vitória/ES");
+            Console.Write("Com qual Transporte você iria: [A]Avião | [C]Carro | [O]Ônibus ");
+            escolha = char.Parse(Console.ReadLine());
+
+            switch(escolha){
+                case 'A' : 
+                    tempo=50;
+                    break;
+                case 'C':
+                    tempo=440;
+                    break;
+                case 'O':
+                    tempo=660;
+                    break;
+                default:
+                    tempo= -1;
+                    break;
+            }
+            if(tempo < 0)
+            {
+                Console.WriteLine("Tansporte Indiponível!");
+                Console.Write("Por vafor escolha um tipo de transporte");
+            }else
+            {
+                Console.Write("Você levará {0} minutos para chegar ao destino", tempo);
+            }
+
+            Console.Write("\nVocê deseja Calcular outro transpote? [S/N]: ");
+            escolha = char.Parse(Console.ReadLine());
+            if(escolha == 's' || escolha == 'S')
+            {
+                goto inicio;
+            }else
+            {
+                Console.Clear();
+                Console.Write("Fim do Programa");
+            }
+        }
+    }
+}
---
+++ ex07/ex07.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ex07
+{
+    class Exercicio07
+    {
+        static void Main()
+        {
+            int numero=0;
+            char escolha;
+
+            inicio:
+            Console.Clear();
+
+            Console.Write("Informe uma Letra: [A] | [B] | [C]: ");
+            escolha=char.Parse(Console.ReadLine());
+            switch(escolha)
+            {
+                case 'A':
+                    numero = 20;
+                    Console.Write("O número vale {0}\n", numero);
+                    break;
+                case 'B':
+                    numero=30;
+                    Console.Write("O número vale {0}\n", numero);
+                    break;
+                case 'C':
+                    numero=40;
+                    Console.Write("O número vale {0}\n", numero);
+                    break;
+                default:
+                    numero=50;
+                    Console.Write("O número vale {0}\n", numero);
+                    break;
+            }
+            Console.Write("Você deseja continuar? [S/N]: ");
+            escolha=char.Parse(Console.ReadLine());
+            if(escolha == 'S' || escolha == 's')
+            {
+                goto inicio;
+                Console.Clear();
+            }else
+            {
+                Console.Clear();
+                Console.Write("Fim do Programa");
+            }
+        }
+    }
+}
