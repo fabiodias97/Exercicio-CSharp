8ccb32e6df50d74e8a8c9e61f0a8f2e6e1be8538
Fábio  Dias <add.fabio97@gmail.com>
2022-01-23 01:05:15 -0300

    exericios

---
+++ ex09/ex09.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex09
+{
+    class Exercício09
+    {
+        static void Main()
+        { // TRABALHANDO COMESTRUTURA DE REPETIÇÃO "FOR" E "FOREACH"
+            ///////////////////// FOR ////////////////////////////
+            int[] num={11,22,33};
+            for(int i=1;i<=10;i++)
+            {
+                Console.Write(i); // Irá realizar uma contagem de 0 até 9
+            }
+            //////////////////// FOREACH ///////////////////////////
+            foreach(int n in num)
+            {
+                Console.Write("\t"+ n);
+            }
+        }
+    }
+}
---
+++ ex10/ex10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ex10
+{
+    class Exercício10
+    {
+        static void Main()
+        { //TRABALHANDO COM ESTRUTURA DE REPETIÇÃO "WHILE" E "DO WHILE" 
+            
+            ////////////// WHILE ///////////////////////////////////////
+            int password= 123;
+            int passUser;
+            int num=0;
+            while(num<10)
+            {
+                num+=1;
+                Console.WriteLine("Olá " + num);
+            }
+
+            ////////////////// DO WHILE //////////////////////////////
+            do{
+                Console.Clear();
+                Console.Write("Informe sua Senha: ");
+                passUser = int.Parse(Console.ReadLine());
+            }while(password != passUser);
+                Console.WriteLine("Autorizado");
+
+            Console.Write("Fim do Programa");
+        }   
+
+    }
+}
