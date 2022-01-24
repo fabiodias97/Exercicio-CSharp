d643f98a7f3d841961e8ba7004a3cfcd78db74c1
Fábio  Dias <add.fabio97@gmail.com>
2022-01-24 00:12:06 -0300

    exercícios em  C#

---
+++ ex12/ex12.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ 
+ namespace ex12
+ {
+     class Exercício12
+     {
+         static void Main()
+         {
+             int num=10;
+             dobrar(ref num); // PASSAGEM DE VALOR POR REFERÊNCIA
+             Console.Write(num);
+
+         }
+         static void dobrar(ref int valor)
+         {
+            valor*=2;
+         }
+     }
+ }
