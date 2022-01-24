4911e29cffb5e419ee2c84b5d662b9c8205c672c
Fábio  Dias <add.fabio97@gmail.com>
2022-01-23 21:27:07 -0300

    Exercícios em C#

---
+++ ex11/ex11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex11
+{
+    class Exercício11
+    {
+        static void Main()
+        {   // Trabalhando com Métodos
+
+            int valor1,valor2;
+            Console.Write("Informe o Primeiro Valor: ");
+            valor1=int.Parse(Console.ReadLine());  // AGUARDA O USUÁRIO INSERIR UM VALOR
+            Console.Write("Informe o Segundo Valor: ");
+            valor2=int.Parse(Console.ReadLine());
+            calculo(valor1,valor2); // CHAMA O  MÉTODO "CALCULO"
+        }
+        static void calculo(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            Console.WriteLine("O Resultado da soma entre {0} e {1} é igual a {2}", num1,num2,resultado);
+        }
+    }
+}
