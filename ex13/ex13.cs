e5c17d45f26e2089cf3d6e9a454f91f602abbdb5
Fábio  Dias <add.fabio97@gmail.com>
2022-01-25 02:55:27 -0300

    Exercícios em C#

---
+++ ex13/ex13.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex13
+{
+    class Exercício13
+    {
+        static void Main()
+        {
+            // EXERCÍCIO COM MATRIZES
+            int[,] num= new int[4,4]{{10,20,30,40},{50,60,70,80},{100,110,120,130},{140,150,160,170}};
+
+            Console.Write(num[2,2]); // resultado será 120
+        }
+    }
+}
