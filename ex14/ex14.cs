ea458ac9ddabdf12142a0d334fe0765475185175
Fábio  Dias <add.fabio97@gmail.com>
2022-02-01 21:34:22 -0300

    ex

--- ex14/ex14.cs
+++ ex14/ex14.cs
@@ -6,7 +6,19 @@ namespace ex14
     {
         static void Main()
         {
-            
+            int[] vetor1 = new int[5];
+
+            Random random = new Random();
+            for (int i = 0; i < vetor1.length; i++)
+            {
+                vetor1[i] = random.Next(10);
+            }
+
+            Console.write("Elementos do Vetor 1")
+            foreach (int n in vetor1)
+            {
+                Console.write(n);
+            }
         }
     }
 }
