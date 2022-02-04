e2719ebcd1d6c49d377fa68d8fbd2f114c7bace0
Fábio  Dias <add.fabio97@gmail.com>
2022-02-04 20:04:31 -0300

    exercício

--- ex14/ex14.cs
+++ ex14/ex14.cs
@@ -6,18 +6,19 @@ namespace ex14
     {
         static void Main()
         {
-            int res, men;
-            int num1 = 10;
-            int num2 = 30;
-            men = dobrar(num1, num2, out res);
-            Console.Write(men, res);
+            int num1,num2,men,s;
+            num1=10;
+            num2=30;
+            men = dobrar(num1, num2, out s);
+            Console.Write(men + "", s);
         }
-        static void dobrar(int valor1, int valor2, out int resultado){
-            int sub, reslt;
+        static int dobrar(int valor1, int valor2, out int som){
+            int sub;
             sub = valor1 - valor2;
-            result = valor1 + valor2;
+            som = valor1 + valor2;
             return sub;
-            return reslt;
+            return som;
+            
         }
     }
 }
