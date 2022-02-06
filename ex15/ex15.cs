08d367ae11c14dd39a251f24ce369dfe8bf8dc8a
Fábio  Dias <add.fabio97@gmail.com>
2022-02-06 18:02:46 -0300

    exercicio

---
+++ ex15/ex15.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex15
+{
+    class Exercicio15
+    {
+        static void Main(){
+            Soma(10,2);
+        }
+        static void Soma(params int[]n){
+            int res=0;
+            if(n.Length < 1){
+                Console.WriteLine("Nao existem valores a serem Somados");
+            }else if(n.Length < 2){
+                Console.WriteLine("Valores insuficientes para Soma: {0}",n[0]);
+            }else{
+                for(int i=0; i < n.Length; i++){
+                    res+=n[i];
+                }
+                Console.Write("A soma dos Valores é: {0}", res);
+            }
+        }
+    }
+}
