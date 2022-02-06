f3cc8198ebc1f1edc9d73f0d8439f772185dbb6b
Fábio  Dias <add.fabio97@gmail.com>
2022-02-06 09:26:09 -0300

    Exercício

--- ex14/ex14.cs
+++ ex14/ex14.cs
@@ -6,19 +6,17 @@ namespace ex14
     {
         static void Main()
         {
-            int num1,num2,men,s;
+            int num1,num2,adic,subt;
             num1=10;
             num2=30;
-            men = dobrar(num1, num2, out s);
-            Console.Write(men + "", s);
+            adic=Math(num1, num2, out subt);
+            Console.Write("{0} Adição e {1} Subtração ", subt, adic);
         }
-        static int dobrar(int valor1, int valor2, out int som){
+        static int Math(int valor1, int valor2, out int som){
             int sub;
-            sub = valor1 - valor2;
-            som = valor1 + valor2;
+            sub=valor1-valor2;
+            som=valor1+valor2;
             return sub;
-            return som;
-            
         }
     }
 }
