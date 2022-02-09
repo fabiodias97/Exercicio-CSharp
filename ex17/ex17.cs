0649876824802a238d171ecf6c52dada7c67b888
Fábio  Dias <add.fabio97@gmail.com>
2022-02-08 22:48:34 -0300

    ex

---
+++ ex17/ex17.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex17
+{
+    class Aluno
+    {
+        static public bool alerta;
+        public string nome;
+        public Aluno(string n){
+            alerta=false;
+            nome=n;
+        }
+        public void info(){
+            Console.Write(nome);
+            Console.WriteLine(alerta);
+        }
+    }
+    class Ex17
+    {
+        static void Main(){
+            Aluno a1=new Aluno("Fabio ");
+            Aluno a2=new Aluno("João ");
+            Aluno a3=new Aluno("Mike ");
+
+            Aluno.alerta=true;
+
+            a1.info();
+            a2.info();
+            a3.info();
+        }
+    }
+}
